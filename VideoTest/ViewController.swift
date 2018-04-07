07dc45858b01250889c2f50801bc9b57f868f179
vinayakparmar92 <vinayakparmar1992@gmail.com>
2018-04-07 19:46:21 +0530

    Initial Commit

---
+++ VideoTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoTest
+//
+//  Created by Vinayak Parmar on 07/04/18.
+//  Copyright © 2018 VMP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
