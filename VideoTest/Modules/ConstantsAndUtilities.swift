17a41e58535427589629f7505dffb605bb04ef90
vinayakparmar92 <vinayakparmar1992@gmail.com>
2018-04-15 17:15:05 +0530

    1) Background image added 2) Category added to put a crater in UIView

--- VideoTest/Modules/ConstantsAndUtilities.swift
+++ VideoTest/Modules/ConstantsAndUtilities.swift
@@ -12,6 +12,7 @@ import UIKit
 let kVideoName  = "VerticalVideo"
 let kVideoType  = "mp4"
 let kAnimationDuration = 0.35
+let kFastAnimationDuration = 0.20
 let kVideoCollapsedFrameSize = 100
 let kPrimaryColor = UIColor.init(red: 142/255,
                                  green: 68/255,
@@ -20,4 +21,27 @@ let kPrimaryColor = UIColor.init(red: 142/255,
 
 
 // MARK: Helper methods
-
+extension UIView {
+    var frameForIdentityTransform : CGRect {
+        let size = bounds.size;
+        let frameForTransformIdentity = CGRect.init(x: center.x - size.width / 2, y: center.y - size.width / 2,
+                                                    width: size.width, height: size.height)
+        return frameForTransformIdentity
+    }
+    
+    func putCraterOf(radius: CGFloat, circleCenter: CGPoint) {
+        let path = CGMutablePath()
+        path.addArc(center: circleCenter,
+                    radius: radius,
+                    startAngle: 0.0,
+                    endAngle: 2.0 * .pi,
+                    clockwise: false)
+        path.addRect(bounds)
+        
+        let maskLayer = CAShapeLayer()
+        maskLayer.path = path
+        maskLayer.fillRule = kCAFillRuleEvenOdd
+        layer.mask = maskLayer
+        clipsToBounds = true
+    }
+}
