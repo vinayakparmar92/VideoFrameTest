58de4f8d9ffdcfb3533a16e07c3cf7ffdd714df1
vinayakparmar92 <vinayakparmar1992@gmail.com>
2018-04-15 15:45:01 +0530

    View mode integrated in Homepage screen

--- VideoTest/Modules/Home screen/HomePageVC.swift
+++ VideoTest/Modules/Home screen/HomePageVC.swift
@@ -31,6 +31,7 @@ class HomePageVC: UIViewController {
         }
     }
     var videoPlayer : AVPlayer?
+    var viewModel = HomePageVM()
     var videoPlayerLayer : AVPlayerLayer?
     var isCollapsedState = false {
         didSet {
@@ -49,24 +50,13 @@ class HomePageVC: UIViewController {
             }
         }
     }
-    let questions = ["Can we Overload or Override static methods in java?",
-                     "Why the main method is static in java?",
-                     "What happens if you remove static modifier from the main method?",
-                     "What is the scope of variables in Java in following cases?",
-                     "What is “this” keyword in java?",
-                     "What is “this” keyword in java? \n What is “this” keyword in java?",
-                     "What is “this” keyword in java? \n What is “this” keyword in java? \n What is “this” keyword in java? \n What is “this” keyword in java? \n ",
-                     "What is an abstract class? How abstract classes are similar or different in Java from C++?",
-                     "What is an abstract class? How abstract classes are similar or different in Java from C++?",
-                     "Which class is the superclass for every class ?",
-                     "Why method overloading is not possible by changing the return type in java?"]
-
     
     // MARK: METHODS
     // MARK: View lifecycle
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
+        // Init video
         playVideoWith(name: kVideoName,
                       ofFormat: kVideoType)
     }
@@ -75,7 +65,7 @@ class HomePageVC: UIViewController {
     @IBAction func tappedShowQuestion(_ sender: UIButton) {
         
         // Present question popup
-        let randomQuestionText = questions[Int(arc4random_uniform(UInt32(questions.count)))]
+        let randomQuestionText = viewModel.questions[Int(arc4random_uniform(UInt32(viewModel.questions.count)))]
         
         let questionAlertVC = QuestionAlertVC.getQuestionAlertWith(questionText: randomQuestionText)
         questionAlertVC.modalPresentationStyle = .overCurrentContext
@@ -96,7 +86,7 @@ class HomePageVC: UIViewController {
                 }
             }
         }
-        
+                
         present(questionAlertVC,
                 animated: true,
                 completion: nil)
@@ -146,4 +136,3 @@ class HomePageVC: UIViewController {
         }
     }
 }
-
---
+++ VideoTest/Modules/Home screen/HomePageVM.swift
@@ -0,0 +1,23 @@
+//
+//  HomePageVM.swift
+//  VideoTest
+//
+//  Created by Vinayak Parmar on 15/04/18.
+//  Copyright © 2018 VMP. All rights reserved.
+//
+
+import UIKit
+
+class HomePageVM: NSObject {
+    let questions = ["Can we Overload or Override static methods in java?",
+                     "Why the main method is static in java?",
+                     "What happens if you remove static modifier from the main method?",
+                     "What is the scope of variables in Java in following cases?",
+                     "What is “this” keyword in java?",
+                     "What is “this” keyword in java? \n What is “this” keyword in java?",
+                     "What is “this” keyword in java? \n What is “this” keyword in java? \n What is “this” keyword in java? \n What is “this” keyword in java? \n ",
+                     "What is an abstract class? How abstract classes are similar or different in Java from C++?",
+                     "What is an abstract class? How abstract classes are similar or different in Java from C++?",
+                     "Which class is the superclass for every class ?",
+                     "Why method overloading is not possible by changing the return type in java?"]
+}
